33c8ebfed54b924a849649312bc90ca8866760e0
Nikita Nikitin <mister.nikneim2013@yandex.ru>
2020-01-26 12:12:31 +0300

    Divided into components

---
+++ social-network/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://image.winudf.com/v2/image1/Y29tLnJhaW4uY29hc3RfaWNvbl8xNTYyMzIwMTI4XzAxMA/icon.png?w=100&fakeurl=1" alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ social-network/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main className="content">
+            <div>
+                <img src="http://fsaea.org/wp-content/uploads/2013/08/whatwedo.jpg" alt="title" />
+            </div>
+            <div>
+                ava + description
+        </div>
+            <div>
+                Posts
+          <div>
+                    post one
+          </div>
+                <div>
+                    post two
+          </div>
+                <div>
+                    post three
+          </div>
+            </div>
+        </main>
+    )
+}
+
+export default Profile;
